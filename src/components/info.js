e092b267bd6b7bfe94b97b807de0e6569e548c3b
SOMA UDAY KIRAN <107768879+Uday283@users.noreply.github.com>
2023-10-14 14:22:40 +0530

    first commit

--- src/components/info.js
+++ src/components/info.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import {useState,useEffect,Component} from 'react';
+import axios from 'axios';
 import './style.css';
 const Details=()=>{
   const[users,setUsers]=useState([]);
